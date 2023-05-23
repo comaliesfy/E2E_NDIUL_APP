b89e070935128a5d4f499e4e0a05e336dcae3c80
alinaorlova <alinaorlova@lamoda.ru>
2023-05-23 16:43:59 +0200

    Remove URL info

--- src/main/java/Configs/URLPages.java
+++ src/main/java/Configs/URLPages.java
@@ -2,11 +2,7 @@ package Configs;
 
 public class URLPages {
 
-    public static final String STAND = "http://sber-ndkn-0.vm.cmx.ru:9080/"; //смена стенда по ссылке. Другие не трогать!
-    //http://sber-ndkn-0.vm.cmx.ru:9080/
-    //http://sber-ndkn-2.vm.cmx.ru:9081/
-    //http://sber-ndkn-1.vm.cmx.ru:9080/
-
+    public static final String STAND = "http://test/";
     public static final String BASE_URL = STAND + "tax-sunr/sbr-ndkn/login.html";
     public static final String URL_home = STAND + "tax-sunr/sbr-ndkn/#/";
     public static final String URL_endpoint = STAND + "tax-sunr/sbr-ndkn/rest/";
